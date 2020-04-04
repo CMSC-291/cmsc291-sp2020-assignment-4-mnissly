6768e31b4c4036ba2426cee01146d5177dadebe9
Micah Nissly <mnissly@gmail.com>
2020-04-04 14:44:55 -0400

    Completed part 1 of Django tutorial

---
+++ README.md
@@ -0,0 +1,2 @@
+# sp-2020 Assigment 4
+django tutorial
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\mniss\PycharmProjects\cmsc291-sp2020-assignment-4-mnissly\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
