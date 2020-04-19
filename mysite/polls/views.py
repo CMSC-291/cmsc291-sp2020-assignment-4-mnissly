6370b6ad07e91498fa36da046de79627db4a19e3
Micah Nissly <mnissly@gmail.com>
2020-04-19 09:38:45 -0400

    added form for adding question and started working on delete_question process

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -42,3 +42,9 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+def remove_question(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.save()
+    return HttpResponseRedirect(reverse('polls:index'))
